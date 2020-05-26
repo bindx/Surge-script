9bf672b05562d99d525a47e9d71c17092445a1d2
Bindx <bindx@LinkedME-2.local>
2020-05-26 19:30:05 +0800

    remove mitm

--- Fit-vip.js
+++ Fit-vip.js
@@ -4,8 +4,6 @@
 [Script]
 FitVip = type=http-response,pattern=^http:\/\/*.*\/SFitWeb\/sfit\/getUserBaseInfo,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/bindx/Surge-script/master/Fit-vip.js,script-update-interval=0
 
-[MITM]
-hostname:bea.sportq.com
 */
 
 var body = $response.body;
