ecf2e5cdc40b6642c960b4586fa19ee788ee89d4
Bindx <bindx@LinkedME-2.local>
2020-05-26 17:04:15 +0800

    update url

--- Fit-vip.js
+++ Fit-vip.js
@@ -3,7 +3,7 @@
 
 [Script]
 
-http-response ^https:\/\/bea\.sportq\.com\/SFitWeb\/sfit\/getUserBaseInfo,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/bindx/Surge-script/master/Fit-vip.js
+FitVip = type=http-response,pattern=^https:\/\/bea\.sportq\.com\/SFitWeb\/sfit\/getUserBaseInfo,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/bindx/Surge-script/master/Fit-vip.js,script-update-interval=0
 
 [MITM]
 hostname:bea.sportq.com
